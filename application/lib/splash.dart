01d4593421b1ee9b71421f05249c9ce45df51830
Cameron MacLean SD2024 <cameron.maclean.2019@uni.strath.ac.uk>
2025-03-13 14:32:44 +0000

    Edit splash.dart

--- application/lib/splash.dart
+++ application/lib/splash.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'page_animation.dart';
-import 'create.dart';
-import 'login.dart';
 
 void main() async {
     WidgetsFlutterBinding.ensureInitialized();
