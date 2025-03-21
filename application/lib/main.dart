cfd6ae5da4887f7db6daf5febb9a019f4a111899
Cameron-Maclean080601 <camimac@hotmail.co.uk>
2025-03-21 14:43:54 +0000

    added comments to API class and main page

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -16,20 +16,20 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( // set material app here rather than inheriting such that app bar settings can be changed 
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 77, 175, 255)),
-      useMaterial3: true,
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          foregroundColor: Colors.blue,
-          backgroundColor: Colors.black,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20), 
-            side: BorderSide.none
+        useMaterial3: true,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.blue,
+            backgroundColor: Colors.black,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20), 
+              side: BorderSide.none
+            )
           )
         )
-      )
       ),
       home: const HomePage(),
     );
@@ -44,10 +44,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final GeminiAIService geminiService = GeminiAIService();
-  Map<String, dynamic> response = {"Response" : "no : response"} ;
+  final GeminiAIService geminiService = GeminiAIService(); // get an instantiation of the AI call class so the method can be called off it
+  Map<String, dynamic> response = {"Response" : "no : response"} ; // set an base value map that can be changed by function
 
-  void generateQuestions() async {
+  void generateQuestions() async { //set state of function ready to be called
     Map<String, dynamic> questionResponse = await geminiService.generateQuestions("Beginner","Python");
     setState(() {
       response = questionResponse;
@@ -59,6 +59,8 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: Stack(
         children: <Widget>[
+          
+          // Sets background and fill the screen
           Positioned.fill(
             child: Container(
               decoration: BoxDecoration(
@@ -69,98 +71,125 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
+
+          // set scrolling widget with padding 
           SingleChildScrollView(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 15.0),
                 child:Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(height: 135),
-                  Container(
-                    height: 250,
-                    decoration: BoxDecoration(
-                      border: Border.all(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+
+                    // adding template container for daily question pop-up
+                    SizedBox(height: 135),
+                    Container(
+                      height: 250,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: const Color.fromARGB(255, 77, 175, 255),
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(25),
+                        color: Color.fromARGB(81, 255, 255, 255),
+                      ),
+                    ),
+
+                    // adding white space
+                    SizedBox(
+                      height:10
+                    ),
+
+                    // adding template container for ----
+                    Container(
+                      height: 150,
+                      decoration: BoxDecoration(
+                        border: Border.all(
                         color: const Color.fromARGB(255, 77, 175, 255),
                         width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(25),
+                        color: Color.fromARGB(81, 255, 255, 255),
                       ),
-                      borderRadius: BorderRadius.circular(25),
-                      color: Color.fromARGB(81, 255, 255, 255),
+                      child: Column(children: <Widget>[
+                        ElevatedButton(
+                          onPressed: generateQuestions, 
+                          child: Text("Generate")
+                        ),
+                        Text(jsonEncode(response))
+                        ],
+                      )
                     ),
-                  ),
-                  SizedBox(
-                    height:10
+
+                    // adding white space
+                    SizedBox(
+                      height:10
                     ),
-                  Container(
-                    height: 150,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: const Color.fromARGB(255, 77, 175, 255),
-                        width: 2,
+
+                    // adding template container for ----
+                    Container(
+                      height: 150,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: const Color.fromARGB(255, 77, 175, 255),
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(25),
+                        color: Color.fromARGB(81, 255, 255, 255),
                       ),
-                      borderRadius: BorderRadius.circular(25),
-                      color: Color.fromARGB(81, 255, 255, 255),
                     ),
-                    child: Column(children: <Widget>[
-                      ElevatedButton(onPressed: generateQuestions, child: Text("Generate")),
-                      Text(jsonEncode(response))
-                    ],
-                    )
-                  ),
-                  SizedBox(
-                    height:10
+
+                    // adding white space
+                    SizedBox(
+                      height:10
                     ),
-                  Container(
-                    height: 150,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: const Color.fromARGB(255, 77, 175, 255),
-                        width: 2,
+
+                    // adding template container for ----
+                    Container(
+                      height: 250,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: const Color.fromARGB(255, 77, 175, 255),
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(25),
+                        color: Color.fromARGB(81, 255, 255, 255),
                       ),
-                      borderRadius: BorderRadius.circular(25),
-                      color: Color.fromARGB(81, 255, 255, 255),
                     ),
-                  ),
-                  SizedBox(
-                    height:10
-                  ),
-                  Container(
-                    height: 250,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: const Color.fromARGB(255, 77, 175, 255),
-                        width: 2,
+
+                    // adding white space
+                    SizedBox(
+                      height:10
+                    ),
+                    
+                    // adding template container for ----
+                    Container(
+                      height: 150,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: const Color.fromARGB(255, 77, 175, 255),
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(25),
+                        color: Color.fromARGB(81, 255, 255, 255),
                       ),
-                      borderRadius: BorderRadius.circular(25),
-                      color: Color.fromARGB(81, 255, 255, 255),
                     ),
-                  ),
-                  SizedBox(
-                    height:10
-                  ),
-                  Container(
-                    height: 150,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: const Color.fromARGB(255, 77, 175, 255),
-                        width: 2,
-                      ),
-                      borderRadius: BorderRadius.circular(25),
-                      color: Color.fromARGB(81, 255, 255, 255),
+
+                    // Container to seperate from Nav Bar
+                    Container(
+                      height: 150,
                     ),
-                  ),
-                  Container(
-                    height: 150,
-                  ),
                 ],
               ),
             )
           ),
+
+          // App Bar
           Positioned(
             top: 0,
             left: 0,
             right: 0,
-            child: AppBar(
+            child: AppBar( // AppBar variables set
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.vertical(
                   bottom: Radius.circular(25),
@@ -179,7 +208,9 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                   SizedBox(width: 110),
-                  IconButton(
+
+                  // Button for showing account detials
+                  IconButton( 
                     icon: Icon(
                       Icons.account_circle,
                       size: 35,
@@ -193,6 +224,8 @@ class _HomePageState extends State<HomePage> {
               ),
             )
           ),
+
+          // Navigation Bar
           Positioned(
             bottom: 50,
             left: 60,
@@ -207,24 +240,32 @@ class _HomePageState extends State<HomePage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
+
+                  // Button for Home Page
                   IconButton(
                     icon: Icon(Icons.home, color: Colors.white),
                     onPressed: () {
                       Navigator.of(context).push(createPageRoute2(Home()));
                     },
                   ),
+
+                  // Button for Progress Page
                   IconButton(
                     icon: Icon(Icons.timeline, color: const Color.fromARGB(75, 255, 255, 255)),
                     onPressed: () {
                       Navigator.of(context).push(createPageRoute2(Progress()));
                     },
                   ),
+
+                  // Button for Leader Boards Page
                   IconButton(
                     icon: Icon(Icons.leaderboard, color: Color.fromARGB(75, 255, 255, 255)),
                     onPressed: () {
                       //Navigator.of(context).push(createPageRoute2(Leaderboard()));
                     },
                   ),
+
+                  // Button for Settings Page
                   IconButton(
                     icon: Icon(Icons.settings, color: Color.fromARGB(75, 255, 255, 255)),
                     onPressed: () {
