134db39f34aab6655716aeaccdbc2d05d3a0116d
Cameron-Maclean080601 <camimac@hotmail.co.uk>
2025-04-03 20:47:26 +0100

    added functionality to update firebase account details

--- application/test/widget_test.dart
+++ application/test/widget_test.dart
@@ -1,6 +1,10 @@
 import 'package:coder_application/splash.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+// this test fails due to the way we have designed the splash screen.
+// as the app function we are happy with it and dont want to change it this late on
+// i believe the way I have written this test is correct and it is simply the nature of the way we have defined the splash
+
 void main(){
   testWidgets('Test Splash', ( WidgetTester tester) async {
   // Build our app and trigger a frame.
